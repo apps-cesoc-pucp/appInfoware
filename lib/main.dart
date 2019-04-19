54d60006419f6c3f9d5128177be78e70f74019a3
lanre97 <flavioontaneda@gmail.com>
2019-04-19 15:45:24 -0500

    Ignorar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:app_infoware/Screens/Home.dart';
+import 'package:app_infoware/Screens/HomeScreen.dart';
 import 'package:flutter/services.dart';
 
 void main(){
